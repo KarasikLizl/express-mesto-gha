f533aa34cdd4e1f69002fd0059cb9e39162fcb51
Elizaveta Kruchinina <Kruchinina.elizaveta1@yandex.ru>
2023-01-10 10:36:14 +0300

    Исправление оишбки regex

--- constants/validator.js
+++ constants/validator.js
@@ -1,1 +1,1 @@
-export const urlRegExp = new RegExp(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi);
+export const urlRegExp = new RegExp(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
